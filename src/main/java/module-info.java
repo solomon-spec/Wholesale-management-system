c3253bf5abf8c17729d10021f3332dc7195c571b
solomon-spec <solomonabate18@gmail.com>
2024-01-21 07:38:24 +0300

    Export DAO

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -8,6 +8,7 @@ module com.example.wholesalemanagmentsystem {
     exports com.example.wholesalemanagementsystem.controllers to javafx.fxml;
     exports com.example.wholesalemanagementsystem;
     exports com.example.wholesalemanagementsystem.dao;
+    exports com.example.wholesalemanagementsystem.models;
     opens com.example.wholesalemanagementsystem.dao to javafx.fxml;
     opens com.example.wholesalemanagementsystem.controllers to javafx.fxml;
     opens com.example.wholesalemanagementsystem.models to javafx.base;
